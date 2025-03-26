85410945cd1bcf91e3cbe24015f558741347af76
sojiro takagi <soujirou.takagi@gmail.com>
2025-03-26 23:20:30 +0900

    テストデータ更新

--- my-app/resources/views/layouts/app.blade.php
+++ my-app/resources/views/layouts/app.blade.php
@@ -16,8 +16,6 @@
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen">
-            @include('layouts.navigation')
-
             <!-- Page Heading -->
             @isset($header)
                 <header class="bg-white dark:bg-gray-800 shadow">
