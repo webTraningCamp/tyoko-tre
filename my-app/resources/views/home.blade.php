a7b19d7a3dd9cd96bcd2a9ca16c2dbadfebd4a9c
sojiro takagi <soujirou.takagi@gmail.com>
2025-03-27 17:01:52 +0900

    home追加

---
+++ my-app/resources/views/home.blade.php
@@ -0,0 +1,181 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="reset.css" rel="stylesheet" />
+    <link href="home.css" rel="stylesheet" type="text/css" media="all">
+    <title>home</title>
+</head>
+<body>
+    <div class="head">
+        <img src="image/headerCircle.png" class="headerCircle">
+    </div>
+
+    <div class="speechBubble font-14">
+        <div>今日は何しよう？</div>
+    </div>
+
+    <img src="image/namakeHeadCenter.png" class="namakeHead">
+
+    <div class="queSelect">
+        <div class="resetFlex">
+            <div class="font-12 todayQue">今日のクエスト</div>
+            <img src="image/resetButton.png" class="resetButton">
+        </div>
+        <div class="font-12 queContent">
+            <img src="image/checkbox/off.png" class="checkOff">
+            <div class="queText">ウィンドウショッピング</div>
+        </div>
+        <div class="font-12 queContent">
+            <img src="image/checkbox/off.png" class="checkOff">
+            <div class="queText">犬の散歩</div>
+        </div>
+        <div class="font-12 queContent">
+            <img src="image/checkbox/off.png" class="checkOff">
+            <div class="queText">歯磨きしながら爪先立ち</div>
+        </div>
+        <div class="font-12 queSelectButton" class="checkOff">
+            <div>選択した内容ではじめる</div>
+        </div>
+    </div>
+    
+    <div class="calenderOutside">
+        <div class="calenderInside">
+            <div class="calenderImgCluster">
+                <div>
+                <div class="nowMonth">
+                    4月
+                </div>
+                   <img src="image/sakuraNamake.png" class="monthImg">
+            </div>
+            </div>
+            <div class="calenderInside"></div>
+            
+        </div>
+
+
+        <div class="calenderAbout">
+            <div class="weeks"></div>
+            <div class="calenderWeeks">
+                <div class="calenderDay">30</div>
+                <div class="calenderDay">31</div>
+                <div class="calenderDay">1</div>
+                <div class="calenderDay">2</div>
+                <div class="calenderDay">3</div>
+                <div class="calenderDay">4</div>
+                <div class="calenderDay weekMon">5</div>
+                <div class="calenderDay weekSat">6</div>
+            </div>
+            <div class="calenderWeeks">
+                <div class="calenderDay">7</div>
+                <div class="calenderDay">8</div>
+                <div class="calenderDay">9</div>
+                <div class="calenderDay">10</div>
+                <div class="calenderDay">11</div>
+                <div class="calenderDay weekMon">12</div>
+                <div class="calenderDay weekSat">13</div>
+                <div class="calenderDay">14</div>
+            </div>
+            <div class="calenderWeeks">
+                <div class="calenderDay">15</div>
+                <div class="calenderDay">16</div>
+                <div class="calenderDay">17</div>
+                <div class="calenderDay">18</div>
+                <div class="calenderDay weekMon">19</div>
+                <div class="calenderDay weekSat">20</div>
+                <div class="calenderDay">21</div>
+                <div class="calenderDay">22</div>
+            </div>
+            <div class="calenderWeeks">
+                <div class="calenderDay">23</div>
+                <div class="calenderDay">24</div>
+                <div class="calenderDay">25</div>
+                <div class="calenderDay weekMon">26</div>
+                <div class="calenderDay weekSat">27</div>
+                <div class="calenderDay">28</div>
+                <div class="calenderDay">29</div>
+                <div class="calenderDay">30</div>
+            </div>
+            <div class="calenderWeeks">
+                <div class="calenderDay">1</div>
+                <div class="calenderDay">2</div>
+                <div class="calenderDay">3</div>
+                <div class="calenderDay">4</div>
+                <div class="calenderDay">5</div>
+                <div class="calenderDay">6</div>
+                <div class="calenderDay">7</div>
+                <div class="calenderDay">8</div>
+            </div>
+            <div class="calenderWeeks">
+                <div class="calenderDay">9</div>
+                <div class="calenderDay">10</div>
+            </div>
+        </div>
+    </div>
+    <!-- <div class="calender">
+        <div class="font-36 nowMonth">
+            <img src="image/sakuraNamake.png" class="monthImg">
+            <div class="monNum">
+                4月
+            </div>
+        </div>
+        <div class="calenderAbout">
+            <div class="calenderWeeks">
+                <div class="calenderDay">30</div>
+                <div class="calenderDay">30</div>
+                <div class="calenderDay">30</div>
+                <div class="calenderDay">30</div>
+                <div class="calenderDay">30</div>
+                <div class="calenderDay">30</div>
+                <div class="calenderDay">30</div>
+            </div>
+            <div class="calenderWeeks">
+                <div class="calenderDay">30</div>
+                <div class="calenderDay">30</div>
+                <div class="calenderDay">30</div>
+                <div class="calenderDay">30</div>
+                <div class="calenderDay">30</div>
+                <div class="calenderDay">30</div>
+                <div class="calenderDay">30</div>
+            </div>
+            <div class="calenderWeeks">
+                <div class="calenderDay">30</div>
+                <div class="calenderDay">30</div>
+                <div class="calenderDay">30</div>
+                <div class="calenderDay">30</div>
+                <div class="calenderDay">30</div>
+                <div class="calenderDay">30</div>
+                <div class="calenderDay">30</div>
+            </div>
+            <div class="calenderWeeks">
+                <div class="calenderDay">30</div>
+                <div class="calenderDay">30</div>
+                <div class="calenderDay">30</div>
+                <div class="calenderDay">30</div>
+                <div class="calenderDay">30</div>
+                <div class="calenderDay">30</div>
+                <div class="calenderDay">30</div>
+            </div>
+            <div class="calenderWeeks">
+                <div class="calenderDay">30</div>
+                <div class="calenderDay">30</div>
+                <div class="calenderDay">30</div>
+                <div class="calenderDay">30</div>
+                <div class="calenderDay">30</div>
+                <div class="calenderDay">30</div>
+                <div class="calenderDay">30</div>
+            </div>
+            <div class="calenderWeeks">
+                <div class="calenderDay">30</div>
+                <div class="calenderDay">30</div>
+                <div class="calenderDay">30</div>
+                <div class="calenderDay">30</div>
+                <div class="calenderDay">30</div>
+                <div class="calenderDay">30</div>
+                <div class="calenderDay">30</div>
+            </div>
+        </div>
+    </div> -->
+</body>
+</html>
