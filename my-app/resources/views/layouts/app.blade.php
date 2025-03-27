5461e79a993d2d3745d4a1e58ef5abd514e08536
sojiro takagi <soujirou.takagi@gmail.com>
2025-03-27 11:59:55 +0900

    フォント追加

--- my-app/resources/views/layouts/app.blade.php
+++ my-app/resources/views/layouts/app.blade.php
@@ -11,11 +11,15 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
         <script src="https://cdn.tailwindcss.com"></script>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Darumadrop+One&display=swap" rel="stylesheet">
+
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body class="font-sans antialiased">
+    <body class="font-sans antialiased bg-[#FBF3D9]">
         <div class="min-h-screen">
             <!-- Page Heading -->
             @isset($header)
