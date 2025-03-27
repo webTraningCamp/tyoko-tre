200e7bad6d3626e074e197636f8d11b550518814
sojiro takagi <soujirou.takagi@gmail.com>
2025-03-27 11:16:27 +0900

    profileiリンク変更

--- my-app/app/Http/Controllers/ProfileController.php
+++ my-app/app/Http/Controllers/ProfileController.php
@@ -14,9 +14,9 @@ class ProfileController extends Controller
     /**
      * Display the user's profile form.
      */
-    public function edit(Request $request): View
+    public function index(Request $request): View
     {
-        return view('profile.edit', [
+        return view('profile.index', [
             'user' => $request->user(),
         ]);
     }
--- my-app/routes/web.php
+++ my-app/routes/web.php
@@ -17,7 +17,7 @@ Route::get('/history-page', [HistoryController::class, 'show'])->middleware('aut
 
 
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::get('/profile', [ProfileController::class, 'index'])->name('profile.index');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     Route::put('/user/update', [UserController::class, 'updateMultiple'])->name('user.update.multiple');
